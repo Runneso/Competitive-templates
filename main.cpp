5e18253e2c720098faeaf124e3dab70a7af8a352
Runneso <111322016+Runneso@users.noreply.github.com>
2024-02-03 18:30:53 +0300

    main.cpp
    C++

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <bits/stdc++.h>
+
+#include <vector>
+#include <list>
+#include <array>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <tuple>
+#include <string>
+
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <cassert>
+
+ 
+#define ll long long int
+#define ld long double
+ 
+#define inf LONG_LONG_MAX
+#define module 1e9+7
+#define all(x) x.begin(),x.end()
+#define print(x) for(auto element:x) cout <<element << " ";
+ 
+ 
+
+ 
+using namespace std;
+
+//freopen("file.txt","r",stdin);
+//getline(cin,string);
+
+void solution(){
+
+
+}
+
+
+
+int main(){   
+    cout.precision(10),cout.setf(ios::fixed);
+    cin.tie(0), cout.tie(0);
+    ios::sync_with_stdio(0);
+    ll t=1;
+    while (t--)
+        solution();
+    return 0;
+}
