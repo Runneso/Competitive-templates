350e8a91b391aa634acbc4ab0b690e1dc18a716c
Runneso <111322016+Runneso@users.noreply.github.com>
2024-02-21 16:28:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-from sys import stdin, stdout
+from sys import stdin, stdout, setrecursionlimit
+from threading import stack_size
 from math import gcd, lcm, pow
 from collections import deque
 from heapq import heappop, heappush, heapify
 from itertools import product, permutations
-from bisect import bisect_right, bisect_left, insort_right, insort_left
-
+from string import ascii_letters
+from bisect import bisect_right, bisect_left
 
 inf = float("inf")
-module = pow(10, 9) + 7
+module = 1_000_000_000 + 7
+
+
+def cin():
+    return stdin.readline().strip()
+
+
+def cout(value):
+    return stdout.write(str(value))
 
 
 def solve():
-    # input
-    name = stdin.readline().strip()
-    # output
-    stdout.write(f"Hello World, {name}.")
+    pass
 
 
 def main():
+    stack_size(2 ** 26), setrecursionlimit(10 ** 9)
     t = 1
     while t > 0:
         solve()
