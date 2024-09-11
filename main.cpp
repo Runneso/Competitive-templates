3382f1d8ea2f93b4ca7836608978abb676cdf35c
Runneso <111322016+Runneso@users.noreply.github.com>
2024-09-11 16:56:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,10 @@
-#include <iostream>
-#include <fstream>
 #include <bits/stdc++.h>
 
-#include <vector>
-#include <list>
-#include <array>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <unordered_set>
-#include <set>
-#include <unordered_map>
-#include <map>
-#include <tuple>
-#include <string>
+using namespace std;
 
-#include <cmath>
-#include <algorithm>
-#include <numeric>
-#include <cassert>
+#pragma GCC optimize("Ofast,unroll-loops") 
+#pragma GCC target("avx,avx2,avx512,fma") 
 
- 
 #define ll long long int
 #define ld long double
  
@@ -31,26 +15,17 @@
 #define print(x) for(auto element:x) cout <<element << " ";
 #define endl "\n"
 
-
- 
-using namespace std;
-
-//freopen("file.txt","r",stdin);
-//getline(cin,string);
-//num % mod = (num % mod + mod) % mod
-
-
 void solution(){
-    
+
 }
 
-int main(){   
-    cout.precision(16),cout.setf(ios::fixed);
-    cin.tie(0), cout.tie(0);
-    ios::sync_with_stdio(0);
-    ll t=1;
-    // cin>>t;
-    while (t--)
-        solution();
-    return 0;
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  int t =1;
+  // cin>>t;
+  while(t--){
+    solution();
+  }
+
 }
