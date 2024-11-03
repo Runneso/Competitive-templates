4e33c2358280ee2688ebbd9152d14f05424262f7
Runneso <111322016+Runneso@users.noreply.github.com>
2024-11-03 08:20:01 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,30 +2,29 @@
 
 using namespace std;
 
-#pragma GCC optimize("Ofast,unroll-loops") 
-#pragma GCC target("avx,avx2,avx512,fma") 
+// #pragma GCC optimize("Ofast,unroll-loops") 
+// #pragma GCC target("avx,avx2,avx512,fma") 
 
 #define ll long long int
 #define ld long double
- 
+
 #define inf LONG_LONG_MAX
-#define MOD 1e9+7
+#define MOD (1e9+7)
 #define EPS 1e-9
 #define all(x) x.begin(),x.end()
-#define print(x) for(auto element:x) cout <<element << " ";
+#define print(x) for(auto element:x) cout << element << " ";
 #define endl "\n"
 
-void solution(){
-
+void solution() {
 }
 
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0); cout.tie(0);
-  int t =1;
-  // cin>>t;
-  while(t--){
-    solution();
-  }
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin>>t;
+    while (t--) {
+        solution();
+    }
 }
