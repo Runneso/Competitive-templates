c932ea1f54a6848ecfe8facfd405ccb593efc098
Runneso <111322016+Runneso@users.noreply.github.com>
2025-02-22 23:24:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,22 +3,30 @@
 using namespace std;
 
 #pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx,avx2,avx512,fma")
+// #pragma GCC target("avx,avx2,avx512,fma")
 
 #define ll long long int
 #define ld long double
 
+#define PI acos(-1)
 #define inf LONG_LONG_MAX
-#define MOD (1e9+7)
+#define MOD (1'000'000'000+7)
+#define MOD2 (1'000'000'000+9)
 #define EPS 1e-9
 #define all(x) x.begin(),x.end()
 #define print(x) for(auto element:x) cout << element << " ";
 #define endl "\n"
 
+// freopen("file.txt","r",stdin);
+// getline(cin,string);
+// num % mod = (num % mod + mod) % mod
+// rand() % (end - start + 1) + start;
+
 void solution() {
 }
 
 int main() {
+    srand(time(nullptr));
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
