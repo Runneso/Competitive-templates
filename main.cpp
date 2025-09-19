03521666fac19d8ce14c1085e9d7bdb8e74337dd
Runneso <111322016+Runneso@users.noreply.github.com>
2025-09-19 20:36:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
+using namespace __gnu_pbds;
 using namespace std;
+
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #pragma GCC optimize("Ofast,unroll-loops")
 // #pragma GCC target("avx,avx2,avx512,fma")
